099e79bc9a243e598ddac0d8dcd4f43cc1a9984f
NGoedix <goedix@wearehackerone.com>
2025-05-30 00:50:04 +0200

    Fix: Removed old methods

--- core/include/editor/core/utils/ProjectFileUtils.h
+++ core/include/editor/core/utils/ProjectFileUtils.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "editor/core/utils/ProjectFileUtils.h"
 #include "engine/core/utils/FileUtils.h"
 
 #include <string>
@@ -10,11 +9,6 @@ namespace fs = std::filesystem;
 
 class ProjectFileUtils {
 public:
-    static fs::path GetExecutablePath();
-    static fs::path GetFilePath(const std::string& relativePath);
     static fs::path GetDataFilePath(const std::string& relativePath);
     static fs::path GetProjectsBaseFolder();
-    static bool DirectoryExists(const fs::path& path);
-    static bool FileExists(const fs::path& path);
-    static char GetPathSeparator();
 };
